6d58041fbad800d9d39fd9d268254875e3157981
Ryan Henson <rycehe@gmail.com>
2021-01-11 23:30:09 -0600

    trimmed code down, got help with filter and search code

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./style.css"
 
 const EmployeeTable = (props) => {
   return (
--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const URL = "https://randomuser.me/api/?results=100&nat=us"
 
+ // eslint-disable-next-line
 export default {
     getEmployees: function() {
         return axios.get(URL);
